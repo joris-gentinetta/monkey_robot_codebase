1a77ef0b01d9ecce7e0ac1ce0f102d27ff48ff2b
jg_mac <gentinetta.joris@gmail.com>
2024-02-16 12:56:46 +0100

    updated all_servos.py

--- testing/all_servos.py
+++ testing/all_servos.py
@@ -1,15 +1,42 @@
 from adafruit_servokit import ServoKit
-kit = ServoKit(channels=16, frequency=333)
-from sleep import sleep
+from time import sleep
+import argparse
 
-for s in range (16):
-    print(s)
-    sleep(0.5)
-    kit.servo[s].set_pulse_width_range(900,2200)
-    kit.servo[s].angle = 0
-    sleep(0.2)
-    kit.servo[s].angle = 180
-    sleep(0.2)
+parser = argparse.ArgumentParser(description='Test all servos')
+parser.add_argument('--type', type=str, default=None, help='type of action to perform')
+args = parser.parse_args()
+kit = ServoKit(channels=16, frequency=333)
+
+
+for s in range(16):
+    kit.servo[s].set_pulse_width_range(900, 2200)
     kit.servo[s].angle = 90
-    sleep(0.5)
+
+if args.type == 'sweep':
+    for s in range(16):
+        sleep(0.5)
+        kit.servo[s].angle = 20
+        sleep(0.5)
+        kit.servo[s].angle = 90
+        sleep(0.5)
+        kit.servo[s].angle = 160
+        sleep(0.5)
+        kit.servo[s].angle = 90
+
+
+if args.type == 'wear':
+    while True:
+        for s in range(16):
+            sleep(0.5)
+            kit.servo[s].angle = 20
+        for s in range(16):
+            sleep(0.5)
+            kit.servo[s].angle = 90
+        for s in range(16):
+            sleep(0.5)
+            kit.servo[s].angle = 160
+        for s in range(16):
+            sleep(0.5)
+            kit.servo[s].angle = 90
+
 
